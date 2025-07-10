27fcb61b39a65d45288209e7fcaffb5a29e9d7f3
Michael Geramb <lasso_riefen0m@icloud.com>
2025-07-10 22:09:44 +0200

    Mock driver implementation

--- src/Driver/Midea/MideaDriver.cpp
+++ src/Driver/Midea/MideaDriver.cpp
@@ -50,44 +50,53 @@ unsigned int MideaDriver::getMaximumVertiacalFixPosition() const
 void MideaDriver::setPower(bool power)
 {
    // To Do: Implementation for power control
+   statusFeedback.powerChanged(power);
 }
 
 void MideaDriver::setMode(AirConditionMode mode)
 {
     // To Do: Implementation for mode control
+    statusFeedback.modeChanged(mode);
 }
 
 void MideaDriver::setTargetTemperature(float temperaturCelius)
 {
     // To Do: Implementation for target temperature control
+    statusFeedback.targetTemperatureChanged(temperaturCelius);
 }
 
 void MideaDriver::setFanSpeed(unsigned int speed)
 {
     // To Do: Implementation for fan speed control
+    statusFeedback.fanSpeedChanged(speed);
 }
 
 void MideaDriver::setSwingHorizontal(bool swing)
 {
     // To Do: Implementation for horizontal swing control
+    statusFeedback.swingHorizontalChanged(swing);
 }
 
 void MideaDriver::setSwingVertical(bool swing)
 {
     // To Do: Implementation for vertical swing control
+    statusFeedback.swingVerticalChanged(swing);
 }
 
 void MideaDriver::setSwingHorizontalFixPosition(unsigned int position)
 {
     // To Do: Implementation for horizontal fix position control
+    statusFeedback.swingHorizontalFixPositionChanged(position);
 }
 
 void MideaDriver::setSwingVerticalFixPosition(unsigned int position)
 {
     // To Do: Implementation for vertical fix position control
+    statusFeedback.swingVerticalFixPositionChanged(position);
 }
 
 void MideaDriver::setExternalSensorRoomTemperature(float temperaturCelius)
 {
     // To Do: Implementation for external sensor room temperature control
+    statusFeedback.roomTemperatureChanged(temperaturCelius);
 }
