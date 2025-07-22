ea8744277efd18fc0335d457655031dece433f41
Michael Geramb <lasso_riefen0m@icloud.com>
2025-07-22 20:31:13 +0200

    Serial Sniffer

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,68 @@
 void setup()
 {
 
-    const uint8_t firmwareRevision = 0;
-    openknx.init(firmwareRevision);
-    openknx.addModule(0, openknxNetwork);
-    openknx.addModule(1, openknxLogic);
-    openknx.addModule(2, openknxFunctionBlocksModule);
-    openknx.addModule(3, openknxAircondition);
-    openknx.addModule(6, openknxFileTransferModule);
+    Serial.begin(115200);
+    pinMode(PROG_LED_PIN, OUTPUT);
+    Serial.println("Starting");
+    OPENKNX_AIR_CONDITION_SERIAL.begin(9600, SERIAL_8E1, OPENKNX_AIR_CONDITION_SERIAL_RX, OPENKNX_AIR_CONDITION_SERIAL_TX);
+    Serial1.begin(9600, SERIAL_8E1, 32, 33);
+   
+    
 
-    openknx.setup();
+    // const uint8_t firmwareRevision = 0;
+    // openknx.init(firmwareRevision);
+    // openknx.addModule(0, openknxNetwork);
+    // openknx.addModule(1, openknxLogic);
+    // openknx.addModule(2, openknxFunctionBlocksModule);
+    // openknx.addModule(3, openknxAircondition);
+    // openknx.addModule(6, openknxFileTransferModule);
+
+    // openknx.setup();
 }
 
+bool lastReadFromSerialWlan = false;
+bool lastReadFromSerialKlima = false;
 void loop()
 {
-   openknx.loop();
+    int byte;
+    byte = Serial1.read();
+    if (byte >= 0)
+    {
+        if (!lastReadFromSerialWlan)
+        {
+            digitalWrite(PROG_LED_PIN, HIGH);
+            Serial.println();
+            Serial.println("WLAN: ");
+            lastReadFromSerialWlan = true;
+            
+        }
+        lastReadFromSerialKlima = false;
+        Serial.printf("%02X", byte);
+        OPENKNX_AIR_CONDITION_SERIAL.write(byte);
+    }
+    byte = OPENKNX_AIR_CONDITION_SERIAL.read();
+    if (byte >= 0)
+    {        
+        if (!lastReadFromSerialKlima)
+        {
+            digitalWrite(PROG_LED_PIN, LOW);
+            Serial.println();
+            Serial.println("KLIMA: ");
+            lastReadFromSerialKlima = true;
+        }
+        lastReadFromSerialWlan = false;
+        Serial.printf("%02X", byte);
+        Serial1.write(byte);
+    }
+   //openknx.loop();
 }
 
 void setup1()
 {
-    openknx.setup1();
+   // openknx.setup1();
 }
 
 void loop1()
 {
-    openknx.loop1();
+    // openknx.loop1();
 }
